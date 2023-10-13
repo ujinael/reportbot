594f185bd963471179b67c4816f3f9ff999f4ba8
evgeny_perevalov <ujinael@gmail.com>
2023-10-14 01:32:11 +0300

    В вебхуке добавлена обработка ошибки

--- src/calltouch/webhook/repository/webhook.repository.ts
+++ src/calltouch/webhook/repository/webhook.repository.ts
@@ -1,7 +1,7 @@
 import { AbstractPostRepository } from '@/core';
 import { HttpService } from '@nestjs/axios';
 import { HttpStatus, Injectable, Logger } from '@nestjs/common';
-import { lastValueFrom, map } from 'rxjs';
+import { lastValueFrom, map, throwError } from 'rxjs';
 import { IncommingCalltouchWebhookDto } from '../dto';
 import { Webhook } from '../entities';
 @Injectable()
@@ -11,10 +11,14 @@ export class CallTouchWebhookRepository
   constructor(private readonly httpService: HttpService) {}
   async post(inputDto: IncommingCalltouchWebhookDto) {
     try {
-      const $reposponse = this.httpService.post<Webhook>(
-        '/calltouch_webhook/lead',
-        inputDto,
-      );
+      const $reposponse = this.httpService
+        .post<Webhook>('/calltouch_webhook/lead', inputDto)
+        .pipe(
+          map((value) => {
+            if (value.status === 500)
+              return throwError(() => Error(String(value.data)));
+          }),
+        );
       await lastValueFrom($reposponse);
 
       return HttpStatus.OK;
