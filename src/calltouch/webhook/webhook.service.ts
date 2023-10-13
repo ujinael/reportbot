1247cdffe66f1da2dc935180f66514ea4233e456
evgeny_perevalov <ujinael@gmail.com>
2023-10-14 01:01:29 +0300

    В вебхуке добавлена обработка ошибок

--- src/calltouch/webhook/webhook.service.ts
+++ src/calltouch/webhook/webhook.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { IncommingCalltouchWebhookDto } from './dto/incomming-webhook.dto';
 import { CallTouchWebhookRepository } from './repository/webhook.repository';
 
@@ -6,12 +6,24 @@ import { CallTouchWebhookRepository } from './repository/webhook.repository';
 export class WebhookService {
   constructor(private readonly webhookRepository: CallTouchWebhookRepository) {}
   processIncommingWebhook(incommingWebhookDto: IncommingCalltouchWebhookDto) {
-    if (
-      incommingWebhookDto.leadtype === 'call' &&
-      incommingWebhookDto.callphase === 'callconnected'
-    )
-      return '';
+    try {
+      if (
+        incommingWebhookDto.leadtype === 'call' &&
+        incommingWebhookDto.callphase === 'callconnected'
+      )
+        return '';
 
-    return this.webhookRepository.post(incommingWebhookDto);
+      return this.webhookRepository.post(incommingWebhookDto);
+    } catch (error) {
+      Logger.error(error.message, 'WebhookService.processIncommingWebhook');
+      throw new HttpException(
+        {
+          reason: 'WebhookService.processIncommingWebhook',
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          errorText: (<Error>error).message,
+        },
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
   }
 }
